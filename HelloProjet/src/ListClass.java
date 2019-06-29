71af1ee9df685dc436da36b979767273914752ea
zhangsan <zhangsan@itcast.cn>
2019-06-29 21:43:37 +0800

    提交

---
+++ HelloProjet/src/ListClass.java
@@ -0,0 +1,9 @@
+public class ListClass {
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssssssss");
+        System.out.println("sssssssssssssssssss");
+        System.out.println("sssssssssssssssssss");
+        System.out.println("sssssssssssssssssss");
+        System.out.println("sssssssssssssssssss");
+    }
+}
